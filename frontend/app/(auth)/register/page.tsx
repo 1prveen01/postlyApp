9d23d1343e861d7b4dda396c45387d5590731dc9
user.name like : praveenkumarbermo@gamil.com <praveenkumarbermo@gamil.com>
2025-11-19 19:40:53 +0530

    Fix header text in registration page from "Login" to "Register"

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
          className={`min-h-screen bg-linear-to-r from-blue-100 via-white to-blue-200 flex items-center justify-center`}
        >
          <div className="max-w-md  px-5 w-full">
-           <h1 className="text-3xl font-bold mb-6">Login</h1>
+           <h1 className="text-3xl font-bold mb-6">Register</h1>
            <RegisterForm />
          </div>
        </div>
